9d220194fbbcb4fc616701e3328aaeefe9b0da62
iLilyth <lilyfromsandbox@gmail.com>
2023-04-01 12:04:42 -0400

    Initial upload

---
+++ src/main/java/me/lilyth/ExampleMod.java
@@ -0,0 +1,44 @@
+package me.lilyth;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.*;
+
+@Mod(
+        modid = ExampleMod.MODID,
+        name = ExampleMod.MOD_NAME,
+        version = ExampleMod.VERSION,
+        clientSideOnly = true,
+        acceptedMinecraftVersions = ExampleMod.MC_VERSION
+)
+public class ExampleMod {
+    public static final String MODID = "examplemod";
+    public static final String MOD_NAME = "ExampleMod";
+
+    public static final String VERSION = "1.0";
+    public static final String MC_VERSION = "1.8.9";
+    public static ExampleMod instance;
+    public ExampleMod() {
+        instance=this;
+    }
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        // Initialize and preload modules
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+        System.out.println("DSNE successfully Initialized!");
+    }
+    /////
+
+    public static ExampleMod getInstance() {
+        return instance;
+    }
+}
